857aeb5bd1f2fbb1180a871104e553e2b5dd512d
Tilak Pandya <tilakpandya2@gmail.com>
2023-02-07 15:47:33 -0500

    Week 4.1

--- index.py
+++ index.py
@@ -17,12 +17,7 @@ if not os.path.exists("dataset"):
     os.makedirs("dataset")
 
 net = cv2.dnn.readNetFromCaffe("deploy.prototxt","res10_300x300_ssd_iter_140000_fp16.caffemodel")
-# Model parameters
-in_width = 300
-in_height = 300
-mean = [104, 117, 123]
-# To check how much portion of the picture a face is covering
-conf_threshold = 0.7
+
 ran=random.randint(1,1000)
 # 27 is ESC key value
 while cv2.waitKey(1) != 27:
@@ -35,37 +30,39 @@ while cv2.waitKey(1) != 27:
     frame_width = frame.shape[1]
 
     # Create a 4D blob from a frame. it converts image to numeric multidimentional array.
-    blob = cv2.dnn.blobFromImage(frame, 1.0, (in_width, in_height), mean, swapRB = False, crop = False)
+    blob = cv2.dnn.blobFromImage(frame, 1.0, (300, 300), [104, 117, 123], swapRB = False, crop = False)
     # Run a model
     net.setInput(blob)
     detections = net.forward()
     for i in range(detections.shape[2]):
         confidence = detections[0, 0, i, 2]
-        if confidence > conf_threshold:
-            x_left_bottom = int(detections[0, 0, i, 3] * frame_width)
-            y_left_bottom = int(detections[0, 0, i, 4] * frame_height)
-            x_right_top = int(detections[0, 0, i, 5] * frame_width)
-            y_right_top = int(detections[0, 0, i, 6] * frame_height)
 
-            # A rectangle for the face 
-            cv2.rectangle(frame, (x_left_bottom, y_left_bottom), (x_right_top, y_right_top), (0, 255, 0))
-            label = "Confidence: %.4f" % confidence
-            label_size, base_line = cv2.getTextSize(label, cv2.FONT_HERSHEY_SIMPLEX, 0.5, 1)
+        #check the threshold of confidence
+        if confidence < 0.7:
+            continue
+        x_left_bottom = int(detections[0, 0, i, 3] * frame_width)
+        y_left_bottom = int(detections[0, 0, i, 4] * frame_height)
+        x_right_top = int(detections[0, 0, i, 5] * frame_width)
+        y_right_top = int(detections[0, 0, i, 6] * frame_height)
 
-            # A rectangle for showing confidence percentage
-            cv2.rectangle(frame, (x_left_bottom, y_left_bottom - label_size[1]),
-                                (x_left_bottom + label_size[0], y_left_bottom + base_line),
-                                (255, 255, 255), cv2.FILLED)
-            cv2.putText(frame, label, (x_left_bottom, y_left_bottom),
-                        cv2.FONT_HERSHEY_SIMPLEX, 0.5, (0, 0, 0))
-            
-            # Save the face image in grayscale
-            face_image = cv2.cvtColor(frame[y_left_bottom:y_right_top, x_left_bottom:x_right_top], cv2.COLOR_BGR2GRAY)
-            cv2.imwrite("dataset/face_" + str(ran) +"_" +str(count)+".jpg", face_image)
-            print(count)
-            count=count+1
+        # A rectangle for the face
+        cv2.rectangle(frame, (x_left_bottom, y_left_bottom), (x_right_top, y_right_top), (0, 255, 0))
 
-        cv2.imshow(win_name, frame)
+        label_size, base_line = cv2.getTextSize("Confidence: %.4f" % confidence, cv2.FONT_HERSHEY_SIMPLEX, 0.5, 1)
+
+        # A rectangle for showing confidence percentage
+        cv2.rectangle(frame, (x_left_bottom, y_left_bottom - label_size[1]),
+                            (x_left_bottom + label_size[0], y_left_bottom + base_line),
+                            (255, 255, 255), cv2.FILLED)
+        cv2.putText(frame, "Confidence: %.4f" % confidence, (x_left_bottom, y_left_bottom),
+                    cv2.FONT_HERSHEY_SIMPLEX, 0.5, (0, 0, 0))
+
+        # Save the face image in grayscale
+        face_image = cv2.cvtColor(frame[y_left_bottom:y_right_top, x_left_bottom:x_right_top], cv2.COLOR_BGR2GRAY)
+        cv2.imwrite("dataset/face_" + str(ran) +"_" +str(count)+".jpg", face_image)
+        count=count+1
+
+    cv2.imshow(win_name, frame)
 
 source.release()
 cv2.destroyWindow(win_name)
