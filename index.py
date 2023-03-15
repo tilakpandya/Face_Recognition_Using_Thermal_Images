74952a2dca6cbbbffd4f017057f792c41557ce6c
Tilak Pandya <tilakpandya2@gmail.com>
2023-03-15 12:38:53 -0400

    Week 9.2

--- index.py
+++ index.py
@@ -13,8 +13,8 @@ count = 0
 if len(sys.argv) > 1:
     s = sys.argv[1]
 
-url = "rtsp://admin:Admin12345@192.168.1.142/Streaming/channels/1"
-source = cv2.VideoCapture(0)
+url = "rtsp://admin:Admin12345@192.168.1.142/Streaming/channels/2"
+source = cv2.VideoCapture(url)
 win_name = 'Camera Preview'
 cv2.namedWindow(win_name, cv2.WINDOW_NORMAL)
 
@@ -44,65 +44,66 @@ while cv2.waitKey(1) != 27:
     if count > 100:
         break
     has_frame, frame = source.read()
-    # To flip the video image
-    frame = cv2.flip(frame,1)
-    frame_height = frame.shape[0]
-    frame_width = frame.shape[1]
+    if has_frame is True:
+        # To flip the video image
+        frame = cv2.flip(frame,1)
+        frame_height = frame.shape[0]
+        frame_width = frame.shape[1]
 
-    # Create a 4D blob from a frame. it converts image to numeric multidimentional array.
-    blob = cv2.dnn.blobFromImage(frame, 1.0, (300, 300), [104, 117, 123], swapRB = False, crop = False)
-    # Run a model
-    net.setInput(blob)
-    detections = net.forward()
-    for i in range(detections.shape[2]):
-        confidence = detections[0, 0, i, 2]
+        # Create a 4D blob from a frame. it converts image to numeric multidimentional array.
+        blob = cv2.dnn.blobFromImage(frame, 1.0, (300, 300), [104, 117, 123], swapRB = False, crop = False)
+        # Run a model
+        net.setInput(blob)
+        detections = net.forward()
+        for i in range(detections.shape[2]):
+            confidence = detections[0, 0, i, 2]
 
-        #check the threshold of confidence
-        if confidence < 0.7:
-            continue
-        x_left_bottom = int(detections[0, 0, i, 3] * frame_width)
-        y_left_bottom = int(detections[0, 0, i, 4] * frame_height)
-        x_right_top = int(detections[0, 0, i, 5] * frame_width)
-        y_right_top = int(detections[0, 0, i, 6] * frame_height)
+            #check the threshold of confidence
+            if confidence < 0.7:
+                continue
+            x_left_bottom = int(detections[0, 0, i, 3] * frame_width)
+            y_left_bottom = int(detections[0, 0, i, 4] * frame_height)
+            x_right_top = int(detections[0, 0, i, 5] * frame_width)
+            y_right_top = int(detections[0, 0, i, 6] * frame_height)
 
-        # A rectangle for the face
-        cv2.rectangle(frame, (x_left_bottom, y_left_bottom), (x_right_top, y_right_top), (0, 255, 0))
+            # A rectangle for the face
+            cv2.rectangle(frame, (x_left_bottom, y_left_bottom), (x_right_top, y_right_top), (0, 255, 0))
 
-        label_size, base_line = cv2.getTextSize("Confidence: %.4f" % confidence, cv2.FONT_HERSHEY_SIMPLEX, 0.5, 1)
+            label_size, base_line = cv2.getTextSize("Confidence: %.4f" % confidence, cv2.FONT_HERSHEY_SIMPLEX, 0.5, 1)
 
-        # A rectangle for showing confidence percentage
-        cv2.rectangle(frame, (x_left_bottom, y_left_bottom - label_size[1]),
-                            (x_left_bottom + label_size[0], y_left_bottom + base_line),
-                            (255, 255, 255), cv2.FILLED)
-        cv2.putText(frame, "Confidence: %.4f" % confidence, (x_left_bottom, y_left_bottom),
-                    cv2.FONT_HERSHEY_SIMPLEX, 0.5, (0, 0, 0))
+            # A rectangle for showing confidence percentage
+            cv2.rectangle(frame, (x_left_bottom, y_left_bottom - label_size[1]),
+                                (x_left_bottom + label_size[0], y_left_bottom + base_line),
+                                (255, 255, 255), cv2.FILLED)
+            cv2.putText(frame, "Confidence: %.4f" % confidence, (x_left_bottom, y_left_bottom),
+                        cv2.FONT_HERSHEY_SIMPLEX, 0.5, (0, 0, 0))
 
-        
-        # Convert the face image to a thermal image using cv2.applyColorMap
-        face_image = frame[y_left_bottom:y_right_top, x_left_bottom:x_right_top]
-        if face_image.size != 0:
-            thermal_image = cv2.applyColorMap(cv2.cvtColor(face_image, cv2.COLOR_BGR2GRAY), cv2.COLORMAP_JET)
-        else:
-            continue        
-        rgb_image = frame[y_left_bottom:y_right_top, x_left_bottom:x_right_top] 
+            
+            # Convert the face image to a thermal image using cv2.applyColorMap
+            face_image = frame[y_left_bottom:y_right_top, x_left_bottom:x_right_top]
+            if face_image.size != 0:
+                thermal_image = cv2.applyColorMap(cv2.cvtColor(face_image, cv2.COLOR_BGR2GRAY), cv2.COLORMAP_JET)
+            else:
+                continue        
+            rgb_image = frame[y_left_bottom:y_right_top, x_left_bottom:x_right_top] 
 
-        # Resize the images to the same size
-        rgb_image = cv2.resize(rgb_image, thermal_image.shape[:2][::-1])
+            # Resize the images to the same size
+            rgb_image = cv2.resize(rgb_image, thermal_image.shape[:2][::-1])
 
-        # Convert the thermal image to grayscale and apply a color map
-        thermal_gray = cv2.cvtColor(thermal_image, cv2.COLOR_BGR2GRAY)
-        thermal_color = cv2.applyColorMap(thermal_gray, cv2.COLORMAP_JET)
+            # Convert the thermal image to grayscale and apply a color map
+            thermal_gray = cv2.cvtColor(thermal_image, cv2.COLOR_BGR2GRAY)
+            thermal_color = cv2.applyColorMap(thermal_gray, cv2.COLORMAP_JET)
 
-        # Blend the RGB and thermal images
-        blended = cv2.addWeighted(rgb_image, 0.5, thermal_color, 0.5, 0)
+            # Blend the RGB and thermal images
+            blended = cv2.addWeighted(rgb_image, 0.5, thermal_color, 0.5, 0)
 
-        # Save the blended image
-        cv2.imwrite("dataset/fused_face_" + str(dataset.id) + "_" + str(count) + ".jpg", blended)
+            # Save the blended image
+            cv2.imwrite("dataset/fused_face_" + str(dataset.id) + "_" + str(count) + ".jpg", blended)
 
-        
-        count=count+1
+            
+            count=count+1
 
-    cv2.imshow(win_name, frame)
+        cv2.imshow(win_name, frame)
 
 source.release()
 cv2.destroyWindow(win_name)
--- recognition.py
+++ recognition.py
@@ -37,8 +37,8 @@ for filename in os.listdir(folder_path):
 # Create the face recognition model
 face_recognizer.train(face_images, np.array(face_labels))
 
-url = "rtsp://admin:Admin12345@192.168.1.142/Streaming/channels/1"
-source = cv2.VideoCapture(0)
+url = "rtsp://admin:Admin12345@192.168.1.142/Streaming/channels/2"
+source = cv2.VideoCapture(url)
 win_name = 'Camera Preview'
 cv2.namedWindow(win_name, cv2.WINDOW_NORMAL)
 
@@ -53,53 +53,56 @@ if os.path.isdir(folder_path):
 # 27 is ESC key value
 while cv2.waitKey(1) != 27:
     has_frame, frame = source.read()
-    
-    # To flip the video image
-    frame = cv2.flip(frame,1)
-    frame_height = frame.shape[0]
-    frame_width = frame.shape[1]
+    if has_frame is True:    
+        # To flip the video image
+        frame = cv2.flip(frame,1)
+        frame_height = frame.shape[0]
+        frame_width = frame.shape[1]
 
-    # Detect faces in the current frame
-    blob = cv2.dnn.blobFromImage(frame, 1.0, (300, 300), [104, 117, 123], swapRB = False, crop = False)
-    net.setInput(blob)
-    detections = net.forward()
-    for i in range(detections.shape[2]):
-        confidence = detections[0, 0, i, 2]
+        # Detect faces in the current frame
+        blob = cv2.dnn.blobFromImage(frame, 1.0, (300, 300), [104, 117, 123], swapRB = False, crop = False)
+        net.setInput(blob)
+        detections = net.forward()
+        for i in range(detections.shape[2]):
+            confidence = detections[0, 0, i, 2]
 
-        if confidence < 0.7:
-            continue
-        x_left_bottom = int(detections[0, 0, i, 3] * frame_width)
-        y_left_bottom = int(detections[0, 0, i, 4] * frame_height)
-        x_right_top = int(detections[0, 0, i, 5] * frame_width)
-        y_right_top = int(detections[0, 0, i, 6] * frame_height)
+            if confidence < 0.7:
+                continue
+            x_left_bottom = int(detections[0, 0, i, 3] * frame_width)
+            y_left_bottom = int(detections[0, 0, i, 4] * frame_height)
+            x_right_top = int(detections[0, 0, i, 5] * frame_width)
+            y_right_top = int(detections[0, 0, i, 6] * frame_height)
 
-        # Convert the face image to a thermal image using cv2.applyColorMap
-        thermal_image = cv2.applyColorMap(cv2.cvtColor(frame[y_left_bottom:y_right_top, x_left_bottom:x_right_top], cv2.COLOR_BGR2GRAY), cv2.COLORMAP_JET)
-        
-        rgb_image = frame[y_left_bottom:y_right_top, x_left_bottom:x_right_top] #cv2.imread("dataset/face_" + str(dataset.id) + "_" + str(count) + ".jpg")
+            # Convert the face image to a thermal image using cv2.applyColorMap
+            thermal_image = cv2.applyColorMap(cv2.cvtColor(frame[y_left_bottom:y_right_top, x_left_bottom:x_right_top], cv2.COLOR_BGR2GRAY), cv2.COLORMAP_JET)
+            
+            rgb_image = frame[y_left_bottom:y_right_top, x_left_bottom:x_right_top] #cv2.imread("dataset/face_" + str(dataset.id) + "_" + str(count) + ".jpg")
 
-        # Resize the images to the same size
-        rgb_image = cv2.resize(rgb_image, thermal_image.shape[:2][::-1])
+            # Resize the images to the same size
+            rgb_image = cv2.resize(rgb_image, thermal_image.shape[:2][::-1])
 
-        # Convert the thermal image to grayscale and apply a color map
-        thermal_color = cv2.applyColorMap(cv2.cvtColor(thermal_image, cv2.COLOR_BGR2GRAY), cv2.COLORMAP_JET)
-        # Fuse the RGB and Thermal images
-        blended = cv2.addWeighted(rgb_image, 0.5, thermal_color, 0.5, 0)
+            # Convert the thermal image to grayscale and apply a color map
+            thermal_color = cv2.applyColorMap(cv2.cvtColor(thermal_image, cv2.COLOR_BGR2GRAY), cv2.COLORMAP_JET)
+            # Fuse the RGB and Thermal images
+            blended = cv2.addWeighted(rgb_image, 0.5, thermal_color, 0.5, 0)
 
-        # Recognize the face using the face recognition model
-        gray_image = cv2.cvtColor(blended, cv2.COLOR_BGR2GRAY)
-        label, confidence = face_recognizer.predict(gray_image)
-        label_text = "Unknown"
-        print(confidence)
-        if(confidence < 50):
-            result = df[df["Id"] == int(label)]["Full_Name"].values
-            if len(result) > 0:
-                label_text = str(result[0])
-                    
-        cv2.rectangle(frame, (x_left_bottom, y_left_bottom), (x_right_top, y_right_top), (0, 255, 0))
-        cv2.putText(frame, label_text, (x_left_bottom, y_left_bottom), cv2.FONT_HERSHEY_SIMPLEX, 0.5, (0, 0, 255))
+            # Recognize the face using the face recognition model
+            gray_image = cv2.cvtColor(blended, cv2.COLOR_BGR2GRAY)
+            label, confidence = face_recognizer.predict(gray_image)
+            label_text = "Unknown"
+            print(confidence)
+            #print(label)
+            if(confidence < 50):
+                result = df[df["Id"] == int(label)]["Full_Name"].values
+                print(result)
+                if len(result) > 0:
+                    label_text = str(result[0])
+            cv2.rectangle(frame, (x_left_bottom, y_left_bottom), (x_right_top, y_right_top), (0, 255, 0))
+            cv2.putText(frame, label_text, (x_left_bottom, y_left_bottom), cv2.FONT_HERSHEY_SIMPLEX, 2, (0, 0, 255),2,cv2.LINE_AA)
 
-    cv2.imshow(win_name, frame)
+        cv2.imshow(win_name, frame)
+    else:
+        break
 
 source.release()
 cv2.destroyWindow(win_name)
